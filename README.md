60038aefd1200bf0ada8a7946bff35adffd3203a
Roman Gebrehiwot <rttgg1@gmail.com>
2019-07-16 11:37:36 -0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Lab-11-Component-Based-UI
+
+Number and name of feature: Component based UI
+
+Estimate of time needed to complete: 4hrs
+
+Start time: 9:15
+
+Finish time: 11:15
+
+Actual time needed to complete: 2hrs
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-//TODO: import react fragment
+//DONE: import react fragment
 
 class App extends React.Component {
   
@@ -20,13 +20,13 @@ class App extends React.Component {
             <button onClick={() => this.setState({ clicks: this.state.clicks - 1 })}>click Me (-) </button>
             </React.Fragment>
       
-      //TODO: create counter.
-      //TODO counter should contain state: this.state = { count: 0};
-      //TODO state should be changed based on clicks to the increment/decroment btns.
-      //TODO: state changes should be updated in the p tag.
+      //DONE: create counter.
+      //DONE counter should contain state: this.state = { count: 0};
+      //DONE state should be changed based on clicks to the increment/decroment btns.
+      //DONE: state changes should be updated in the p tag.
       
-    //     {/* TODO add p element to show current number of clicks. Starts at zero *<p>0</p>
-    //     {/* TODO: add buttons, increment and decroment (+, -)  */}
+    //DONE add p element to show current number of clicks. Starts at zero *<p>0</p>
+    //DONE: add buttons, increment and decroment (+, -)  
     //     
     )
   }
